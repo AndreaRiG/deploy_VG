a69dee1a29224fd136cdd9b761458570ea8fb2e1
andrea Rincon <rincondiseno@gmail.com>
2024-02-02 16:26:46 -0500

    new connection bckend

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,8 @@ import LandingPage from './components/LandingPage/LandingPage';
 import Home from './components/Home/Home';
 import Detail from './components/Detail/Detail';
 import Form from './components/Form/Form'
-
+import axios from 'axios';
+axios.defaults.baseURL = 'https://deployvg-production.up.railway.app/';
 
 
 
