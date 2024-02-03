c0263aa4f3204d31a525b411302990a3e28e267e
andrea Rincon <rincondiseno@gmail.com>
2024-02-02 21:37:09 -0500

    update front

--- api/src/app.js
+++ api/src/app.js
@@ -20,7 +20,7 @@ server.use(morgan('dev'));
 server.use((req, res, next) => {
 
  
-  res.header('Access-Control-Allow-Origin', 'https://deploy-vg-andreas-projects-78670ae0.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'https://deploy-vg.vercel.app');
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
